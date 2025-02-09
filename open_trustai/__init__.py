d6f3650e727c68f25676ddc716411702a174f716
Yongkai Wu <yongkaiwu476@gmail.com>
2025-02-08 19:48:30 -0500

    Initialize Open-TrustAI library package with core module imports

---
+++ open_trustai/__init__.py
@@ -0,0 +1,17 @@
+"""Open-TrustAI: A lightweight library for fairness evaluation and bias mitigation in deep learning."""
+
+from open_trustai import datasets
+from open_trustai import losses
+from open_trustai import metrics
+from open_trustai import models
+from open_trustai import utils
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "datasets",
+    "losses",
+    "metrics",
+    "models",
+    "utils",
+]
